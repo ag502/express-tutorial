d5298714fb34f508884aaa3460de77737b930b6d
LeeJaeyun <ag502@naver.com>
2021-07-10 00:12:15 +0900

    feat : page 라우터 추가

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const sanitizeHtml = require('sanitize-html');
 const template = require('./lib/template');
 
 app.get('/', (req, res) => {
@@ -15,6 +16,32 @@ app.get('/', (req, res) => {
       `<a href='/create'>create</a>`
     );
     res.send(html);
+  });
+});
+
+app.get('/page/:pageId', (req, res) => {
+  fs.readdir('./data', function (error, filelist) {
+    const filteredId = req.params.pageId;
+    fs.readFile(`./data/${filteredId}`, 'utf8', (err, description) => {
+      const title = filteredId;
+      const sanitizedTitle = sanitizeHtml(title);
+      const sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ['h1'],
+      });
+      const list = template.list(filelist);
+      const html = template.HTML(
+        sanitizedTitle,
+        list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+                <a href="/update?id=${sanitizedTitle}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${sanitizedTitle}">
+                  <input type="submit" value="delete">
+                </form>`
+      );
+      res.send(html);
+    });
   });
 });
 
