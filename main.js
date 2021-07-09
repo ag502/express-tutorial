dcbbea1b8945513fbfe537c7760d9c2e417f78db
LeeJaeyun <ag502@naver.com>
2021-07-09 23:54:05 +0900

    feat : 홈 라우터 추가

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const template = require('./lib/template');
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => {
+  fs.readdir('./data', (error, files) => {
+    const title = 'Welcome';
+    const description = 'Hello, node.js';
+    const list = template.list(files);
+    const html = template.HTML(
+      title,
+      list,
+      `<h2>${title}</h2>${description}`,
+      `<a href='/create'>create</a>`
+    );
+    res.send(html);
+  });
+});
 
 app.listen(5000, () => console.log('Listening on 5000'));
